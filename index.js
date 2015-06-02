921508e7882138c9ec9b69cc1ea410e985a64a50
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-02 09:08:35 +0100

    Demo

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send(cool('Hello World!'));
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var io = require('socket.io').listen(app.listen(app.get('port')));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
 });
+
