753f1866b193a32aa4a4d91801be024e2319fe35
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-04 14:40:04 +0100

    sdfsdfs

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 
-require('./routes.js')(app, passport); 
+require('./controllers')(app, passport); 
 
 
 
