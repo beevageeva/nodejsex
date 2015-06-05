c614c91497ed1518e7be4be8dbca0e0139a5712c
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-05 14:43:25 +0100

    sdfsdfsf

--- controllers/todos.js
+++ controllers/todos.js
@@ -88,7 +88,20 @@ exports.update = function(req, res) {
 
 
 exports.delete = function(req, res) {
-	  todo.findById(req.body.id, function (err, obj) {
+		console.log("--------------------post-------------------");
+//		console.log(req.headers);
+		console.log("--------------------post-params------------------");
+		console.log(req.params);
+		console.log("--------------------post-prop------------------");
+		console.log(Object.keys(req));
+//		console.log("--------------------post-body------------------");
+//		console.log(req.body);
+//		console.log("--------------------post-query------------------");
+//		console.log(req.query);
+//		console.log("--------------------post-url------------------");
+//		console.log(req.url);
+console.log("--------------------post-end------------------");
+	  todo.findById(req.params.id, function (err, obj) {
     	obj.remove(function (err) {
       if (!err) {
         console.log("removed");
