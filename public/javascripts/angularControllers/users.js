b9bce47697ac565bc461422c936cf4fb7ac06681
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-12 14:24:10 +0100

    sdfsdfsf

--- public/javascripts/angularControllers/users.js
+++ public/javascripts/angularControllers/users.js
@@ -1,41 +1,18 @@
-var userApp = angular.module('userApp', ['reCAPTCHA', 'ngResource'])
-	.config(function (reCAPTCHAProvider) {
-    reCAPTCHAProvider.setPublicKey('6LeKQggTAAAAAJUEnnM9uVhuT_1veQD9mkH5MgkD');
- })
-.factory('Recaptcha', function ($resource) {
-
-        return $resource(
-            '/email/send',
-            {id: '@id'},
-            {
-                send: {
-                    url: '/email/send',
-                    method: 'POST'
-                }
-            }
-        )
-    });
-function mainController(Recaptcha, $scope, $http) {
+var userApp = angular.module('userApp', []).
+function mainController($scope, $http) {
     $scope.formData = {};
 		
     $scope.createUser = function() {
 
-        $http.post('/api/recaptcha', $scope.captchaResponse)
-					.success(function(data){
-		        $http.post('/api/users', $scope.formData)
-		            .success(function(data) {
-		                $scope.formData = {}; // clear the form so our user is ready to enter another
-		                console.log(data);
-		            })
-		            .error(function(data) {
-		                console.log('Error: ' + data);
-		            });
+        $http.post('/api/users', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
 							
-					})
-					.error(function(data) {
-                    console.log('Error: ' + data);
-										grecaptcha.reset();
-          	});
 
 
 
