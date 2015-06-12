5043f19b1acee0e2b78cdb67442221638581f65a
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-12 12:24:28 +0100

    sdfsdfsf

--- public/javascripts/angularControllers/users.js
+++ public/javascripts/angularControllers/users.js
@@ -1,11 +1,13 @@
 var userApp = angular.module('userApp', []);
 function mainController($scope, $http) {
     $scope.formData = {};
+		$scope.recaptchaRegister = grecaptcha.render("recaptchaId", {
+03        'sitekey': '6LeKQggTAAAAAJUEnnM9uVhuT_1veQD9mkH5MgkD'
+04    });
 		
     $scope.createUser = function() {
-				console.log($scope);
 
-        $http.post('/api/recaptcha', $scope.g-recaptcha-response)
+        $http.post('/api/recaptcha', grecaptcha.getResponse($scope.recaptchaRegister))
 					.success(function(data){
 		        $http.post('/api/users', $scope.formData)
 		            .success(function(data) {
