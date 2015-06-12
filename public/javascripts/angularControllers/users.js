795a303ebd82dc1b7f5f98f936cac1a16fc9335f
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-12 12:41:28 +0100

    sdfsdfsf

--- public/javascripts/angularControllers/users.js
+++ public/javascripts/angularControllers/users.js
@@ -1,3 +1,5 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 var userApp = angular.module('userApp', []);
 function mainController($scope, $http) {
     $scope.formData = {};
