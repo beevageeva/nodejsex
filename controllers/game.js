b81bbf5853c1bda5d600b6fbe6fc92ed92c43055
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-19 13:24:09 +0100

    sdfsdfsf

--- controllers/game.js
+++ controllers/game.js
@@ -21,7 +21,7 @@ function shuffle(array) {
 }
 
 function getCards(nPlayers, nCards){
-	var start = 52 - nPlayers * 8;
+	var start = 51 - nPlayers * 8;
 	var end = 52;
 	var cards = [];	
 	for(var i = start; i<=end; i++){
