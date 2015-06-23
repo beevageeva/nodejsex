a569a85059226625cb2c680e1b877aa9355c1ce8
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-23 07:39:35 +0100

    sdfsdfsf

--- controllers/game.js
+++ controllers/game.js
@@ -42,6 +42,7 @@ io.sockets.on('connection', function (socket) {
 					var newRoom = new Room({"name": data.message, "usernames": roomUsernames , "finished": false, "games": []});
 					//nCards = 1 for the first game
 					newRoom.addGame(1);
+					console.log("********cards first game object before save : " + newRoom.games[0].cards);
 				  newRoom.save(function (err) {
 				    if (!err) {
 				      console.log("created");
