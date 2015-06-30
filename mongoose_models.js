b4bc0db2157e60df2cf49ca54901e46c427efa89
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-06-30 13:00:00 +0100

    sdfsdfsf

--- mongoose_models.js
+++ mongoose_models.js
@@ -220,6 +220,9 @@ roomSchema.methods.addMove = function(card, username){
 		username = this.usernames[indexUsername + 1];
 	}	
 	g.moves[g.moves.length - 1].push(card);
+	//TODO mark modified
+	g.markModified("moves");
+	this.markModified("games");
 	console.log("AFTER ADDING");
 	console.log(g.moves[g.moves.length - 1]);	
 	//test if this last move was the last in the game
