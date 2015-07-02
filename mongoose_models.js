e6d941092b102d19620eaadd8417fa24715b5128
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-07-02 13:39:48 +0100

    sdfsdfsf

--- mongoose_models.js
+++ mongoose_models.js
@@ -279,7 +279,7 @@ roomSchema.methods.addMove = function(card, username){
 		}
 		//g.markModified("moves");
 		this.markModified("games");
-		return [username, res, position];
+		return [username, res, position - indexFirstPlayer];
 		
 	//}); //findOne
 
